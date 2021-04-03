dde4a9d899d26892ef337429ea77827dc7c6af69
cacooper21 <58480492+cacooper21@users.noreply.github.com>
2021-04-03 19:40:16 -0400

    JPanel and JButton

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
 import github.tools.client.GitHubApiClient;
 import github.tools.responseObjects.*;
+
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import git.tools.client.GitSubprocessClient;
 
-public class Main {
+public class Main extends JFrame {
     public static void main(String[] args) {
-        System.out.println("Test");
+       
+    	System.out.println("Test");
         GitSubprocessClient gitSubprocessClient = new GitSubprocessClient("insert repo path here");
 
         GitHubApiClient gitHubApiClient = new GitHubApiClient("insert user", "insert token");
         GetRepoInfoResponse repoInfo = gitHubApiClient.getRepoInfo("CSC109", "GitHubApiClient");
+        new Main("test");
+       
     }
+    
+    public Main(String title) {
+
+        super(title);
+        this.setSize(500, 500);
+        this.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        this.add(new Panel());
+        
+        this.setVisible(true);
+      }
+
+   
 }
---
+++ src/Panel.java
@@ -0,0 +1,12 @@
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Panel extends JPanel {
+
+	public Panel() {
+		super();
+		
+		JButton button = new JButton("test");
+		this.add(button);
+	}
+}
